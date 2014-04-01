dde58716b6685215984559ded2a2599fe3d5bca2
Bryce Davis <bad1128@gmail.com>
2014-04-01 14:24:52 -0400

    Add constants for Unicode symbols

---
+++ chess_consts.rb
@@ -0,0 +1,14 @@
+module Chess
+    W_KING   = "\u2654"
+    W_QUEEN  = "\u2655"
+    W_ROOK   = "\u2656"
+    W_BISHOP = "\u2657"
+    W_KNIGHT = "\u2658"
+    W_PAWN   = "\u2659"
+    B_KING   = "\u265A"
+    B_QUEEN  = "\u265B"
+    B_ROOK   = "\u265C"
+    B_BISHOP = "\u265D"
+    B_KNIGHT = "\u265E"
+    B_PAWN   = "\u265F"
+end
